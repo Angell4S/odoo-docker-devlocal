b23f69e2d543e62bbd4a902dfd1f79338e6ac6d5
Angell4S <asencios093@gmail.com>
2022-06-06 23:13:40 -0500

    primer modulo

---
+++ addons-customize/primer_modulo/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Curso de programacion odoo 14',
+    'summary':"""Curso de programacion odoo 14 , funcionalidades""",
+    
+    'author':'Angel Asencios',
+    'category':'General',
+    'version':'1.0.0',
+    'depends':[],
+    'data':[],
+}
